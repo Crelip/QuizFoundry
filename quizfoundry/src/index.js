badf47efc94ffae8148700be3f9eb43ab04a9580
Filip Hostak <hostak7@uniba.sk>
2024-04-07 12:42:15 +0200

    Slightly changed file structure and added initial QuizCreation component with sample data.

--- quizfoundry/src/index.js
+++ quizfoundry/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 ReactDOM.render(
   <Navbar />,
