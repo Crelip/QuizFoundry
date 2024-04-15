7729d35a997314437bfca8a458a7029a39c4285f
Filip Hostak <hostak7@uniba.sk>
2024-04-15 15:47:01 +0200

    Added more info after finishing quiz.

--- quizfoundry/src/components/QuizSolving.js
+++ quizfoundry/src/components/QuizSolving.js
@@ -3,8 +3,12 @@ import React, { useState } from 'react';
 function QuizSolving({ questions }) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const [answeredQuestionsAmount, setAnsweredQuestionsAmount] = useState(0);
+  const [correctAnswersAmount, setCorrectAnswersAmount] = useState(0);
 
-  const handleAnswer = (answer) => {
+  const handleAnswer = (answer, question) => {
+    setAnsweredQuestionsAmount(answeredQuestionsAmount + 1);
+    if(question.correctAnswers.includes(answer)) setCorrectAnswersAmount(correctAnswersAmount + 1);
     const updatedUserAnswers = [...userAnswers, answer];
     setUserAnswers(updatedUserAnswers);
     const currentQuestion = questions[currentQuestionIndex];
@@ -16,10 +20,10 @@ function QuizSolving({ questions }) {
     }
   };
 
-  const handleInputSubmit = (e) => {
+  const handleInputSubmit = (e, question) => {
     e.preventDefault();
     const answer = e.target.answer.value;
-    handleAnswer(answer);
+    handleAnswer(answer, question);
     e.target.reset();
   };
 
@@ -34,20 +38,25 @@ function QuizSolving({ questions }) {
           {currentQuestion.isChoice ? (
             <div>
               {currentQuestion.choiceAnswers.map((choice, index) => (
-                <button key={index} onClick={() => handleAnswer(choice)}>
+                <button key={index} onClick={() => handleAnswer(choice, currentQuestion)}>
                   {choice}
                 </button>
               ))}
             </div>
           ) : (
-            <form onSubmit={handleInputSubmit}>
+            <form onSubmit={(e) => handleInputSubmit(e, currentQuestion)}>
               <input type="text" name="answer" />
               <button type="submit">Submit</button>
             </form>
           )}
         </div>
       )}
-      {!currentQuestion && <p>Quiz completed!</p>}
+      {!currentQuestion && <div><p>Quiz completed!</p>
+      <p>Total questions answered: {answeredQuestionsAmount}</p>
+      <p>Correct answers: {correctAnswersAmount}</p>
+      <p>Success rate: {correctAnswersAmount / answeredQuestionsAmount * 100}%</p>
+      </div>
+      }
     </div>
   );
 }
