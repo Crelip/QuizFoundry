bcd37790d3e95d956a246f21153e0ece8fe86e27
Filip Hostak <hostak7@uniba.sk>
2024-04-16 13:56:06 +0200

    Set up data models.

--- quizfoundry_backend/quizfoundry/models.py
+++ quizfoundry_backend/quizfoundry/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class Quiz(models.Model):
+    quizName = models.CharField(max_length=100)
+    quizOwner = models.PositiveIntegerField()
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    firstQuestion = models.PositiveIntegerField()
+
+class Question(models.Model):
+    quizID = models.PositiveIntegerField()
+    questionText = models.CharField(max_length=300)
+    isChoice = models.BooleanField()
+    multimedia = models.BinaryField()
+    dynamicNext = models.BooleanField()
+
+class ChoiceAnswers(models.Model):
+    questionID = models.PositiveIntegerField()
+    choiceAnswer = models.CharField(max_length=100)
+
+class CorrectAnswers(models.Model):
+    questionID = models.PositiveIntegerField()
+    correctAnswer = models.CharField(max_length=100)
+
+class NextQuestion(models.Model):
+    questionID = models.PositiveIntegerField()
+    answer = models.CharField(max_length=100)
+    nextQuestionID = models.PositiveIntegerField()
+
+class Answer(models.Model):
+    quizID = models.PositiveIntegerField()
+    userID = models.IntegerField()
+    timeSubmitted = models.DateTimeField(auto_now_add=True)
+    correctAnswers = models.PositiveIntegerField()
+    totalQuestions = models.PositiveIntegerField()
+
+class AnswerQuestion(models.Model):
+    answerID = models.PositiveIntegerField()
+    questionID = models.PositiveIntegerField()
+    answer = models.CharField(max_length=100)
+    isCorrect = models.BooleanField()
