295de60b66109559dbdc95827500fa3cbaf0be1e
Filip Hostak <hostak7@uniba.sk>
2024-04-18 14:18:16 +0200

    Implemented proper authentification to be used by frontend.

--- quizfoundry/src/components/Login.js
+++ quizfoundry/src/components/Login.js
@@ -1,46 +1,45 @@
-import React, { useContext, useState } from 'react';
-import AuthContext from './context/AuthContext';
-import QuizCreation from './QuizCreation';
+import React, { useContext, useState } from "react";
+import AuthContext from "./context/AuthContext";
+import QuizCreation from "./QuizCreation";
 
-const Login = ({theme}) => {
-    const { user, loginUser } = useContext(AuthContext);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+const Login = ({ theme }) => {
+  const { user, loginUser } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    //Authentication
-    const handleLogin = (e) => {
-        e.preventDefault();
-        loginUser(username, password);
-    };
+  //Authentication
+  const handleLogin = (e) => {
+    e.preventDefault();
+    loginUser(username, password);
+  };
 
-    return (
+  return (
+    <div>
+      {user ? (
         <div>
-            {user ? (
-                <div>
-                    <p>Signed in as: {user}</p>
-                    <QuizCreation theme={theme}/>
-                </div>
-            ) : (
-                <form onSubmit={handleLogin}>
-                    <input 
-                        type="text" 
-                        name="username" 
-                        placeholder="Enter username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input 
-                        type="password" 
-                        name="password" 
-                        placeholder="Enter password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <input type="submit" value="Login" />
-                </form>
-            )}
+          <QuizCreation theme={theme} />
         </div>
-    );
+      ) : (
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" value="Login" />
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default Login;
--- quizfoundry/src/components/context/AuthContext.js
+++ quizfoundry/src/components/context/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
@@ -9,8 +10,25 @@ export const AuthProvider = ({ children }) => {
   let [authTokens, setAuthTokens] = useState(null);
 
   let loginUser = async (username, password) => {
-    console.log("Logging in");
-    setUser(username);
+    const response = await fetch(process.env.REACT_APP_API_URL + "token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    let data = await response.json();
+    if (data) {
+      if (data.detail === "No active account found with the given credentials")
+        return;
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      setAuthTokens(data);
+      let tmp = jwtDecode(data.access);
+      console.log(tmp);
+      setUser(username);
+    } else {
+      alert("Something went wrong during authentication.");
+    }
   };
 
   let logoutUser = (e) => {
